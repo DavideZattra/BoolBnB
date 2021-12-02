5f6298626f52c13729eb19e2a74e1104efe3e752
Davide Zattra <d.zattra@gmail.com>
2021-12-02 17:31:42 +0100

    views seeder done

---
+++ database/seeds/ViewsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+use App\Models\View;
+
+class ViewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for( $i = 0; $i < 20; $i++ ){
+
+            $newView = new View();
+            $newView->ip_adress = $faker->ipv4();
+            $newView->visited_at = $faker->dateTime();
+            $newView->save();
+        };
+    }
+}
