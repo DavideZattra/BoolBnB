6383dac24212ddf100547e28a3569c19b68b8859
Camilla Manzo <85356516+camillamanzo@users.noreply.github.com>
2021-12-02 15:13:43 +0100

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/davide/Documents/Repository/BoolBnB/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/davide/Documents/Repository/BoolBnB/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/camillasassoli/Desktop/boolean/final_project/BoolBnB/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/camillasassoli/Desktop/boolean/final_project/BoolBnB/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
