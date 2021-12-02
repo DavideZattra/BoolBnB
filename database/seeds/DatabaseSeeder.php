74ad906691a7891352012e5fe791202a4f694d27
Davide Zattra <d.zattra@gmail.com>
2021-12-02 19:11:53 +0100

    UserTableseederDone

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
+            UsersTableSeeder::class,
             MessageTableSeeder::class,
             SponsorTableSeeder::class,
             ViewsTableSeeder::class,
