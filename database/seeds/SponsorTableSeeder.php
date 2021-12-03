945904bfbd5df86534576baee0853bb4b84b700f
Camilla Manzo <85356516+camillamanzo@users.noreply.github.com>
2021-12-03 10:28:55 +0100

    added count() to sponsor seeder

--- database/seeds/SponsorTableSeeder.php
+++ database/seeds/SponsorTableSeeder.php
@@ -16,22 +16,11 @@ class SponsorTableSeeder extends Seeder
         $sponsorPrices = ['19.99', '24.99', '29.99'];
         $sponsorDurations = ['1', '3', '6'];
 
-<<<<<<< HEAD
-        for ($i = 1 ; $i <= 3; $i++){
-
-            $newSponsor = new Sponsor();
-            
-            $newSponsor->name = $sponsorNames[$i-1];
-            $newSponsor->price = $sponsorPrices[$i-1];
-            $newSponsor->duration = $sponsorDurations[$i-1];
-
-=======
         for ($i = 0 ; $i < count($sponsorNames); $i++){
             $newSponsor = new Sponsor();
             $newSponsor->name = $sponsorNames[$i];
             $newSponsor->price = $sponsorPrices[$i];
             $newSponsor->duration = $sponsorDurations[$i];
->>>>>>> amenity
             $newSponsor->save();
         }
     }
