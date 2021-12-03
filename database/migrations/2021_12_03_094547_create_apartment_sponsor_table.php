b3c6393b592dda46a8343e043e62a740c4c0ecc4
Camilla Manzo <85356516+camillamanzo@users.noreply.github.com>
2021-12-03 10:46:58 +0100

    created apartment_sponsor table

---
+++ database/migrations/2021_12_03_094547_create_apartment_sponsor_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentSponsorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_sponsor', function (Blueprint $table) {
+            $table->id();
+            $table->id();
+            $table->unsignedBigInteger('apartment_id');
+            $table->unsignedBigInteger('sponsor_id');
+            
+            // cascade drops the whole row if one element of the two is deleted. (only used in many to many relations)
+            $table->foreign('apartment_id')->references('id')->on('apartments')->onDelete('cascade');
+            $table->foreign('sponsor_id')->references('id')->on('sponsors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_sponsor');
+    }
+}
