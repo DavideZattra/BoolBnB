d96391389ca1ce7ede77ebcb38b310a001d06c6f
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-07 10:35:42 +0100

    aggiunta show di prova

--- resources/views/users/Apartments/show.blade.php
+++ resources/views/users/Apartments/show.blade.php
@@ -1,1 +1,28 @@
-<h1>show</h1>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-12">
+            <h3 class="text-right">
+            <a href="{{ route('users.apartments.index') }}">Torna a apartments</a>
+            </h3>
+        </div>
+        <div class="card col-12">
+            <div class="card-body">
+                <h5 class="card-title text-uppercase font-weight-bold mb-4">{{ $apartment->descriptive_title }}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $apartment->rooms }}</h6>
+                <img class="show-copertina" src="{{ $apartment->image }}" alt="Copertina di {{ $apartment->descriptive_title }}">
+                <p class="card-text">{{ $apartment->description }}</p>
+                <div class="d-flex">
+                    <a href="{{ route('users.apartments.edit', $apartment) }}" class="btn btn-primary mr-5">Modifica</a>
+                    <form action="{{route('users.apartments.destroy', $apartment->id )}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+                        <button class="btn btn-danger" type="submit">Elimina</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
