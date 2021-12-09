e203e0fd98ea793571235258cdfdc42d7cbea757
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-09 11:08:42 +0100

    aggiunto file vue

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="container">
-        <ApartmentsList />
-    </div>
+  <section>
+    <Jubotron />
+    <FakeHouses />
+    <ApartmentList />
+  </section>
 </template>
 
 <script>
+import Jumbotron from './apartments/Jumbotron.vue';
+import FakeHouses from './apartments/FakeHouses.vue';
 import ApartmentsList from './apartments/ApartmentsList.vue';
 
 export default {
     name: "App",
     components: {
+        Jumbotron,
+        FakeHouses,
         ApartmentsList
-    }
+    },
+    data
 }
 </script>
 
