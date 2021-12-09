489961e67edb92c70789839352101386185edac4
Furentsu <eugeniofranzoso@gmail.com>
2021-12-09 17:35:11 +0100

    Cycled apartments in guests' search

--- resources/js/components/apartments/ApartmentCard.vue
+++ resources/js/components/apartments/ApartmentCard.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="row">
-      <div class="col-12">
-          <h1>Card</h1>
-      </div>
+      <div class="card col-3 m-3">
+        <div class="card-body">
+            <img class="img-fluid" :src="apartment.image" :alt="apartment.descriptive_title">
+            <h4 class="card-title">{{apartment.descriptive_title}}</h4>
+            <h5 class="card-title">{{apartment.addresses.country}}</h5>
+            <span class="card-title">{{apartment.addresses.city}}</span>
+            <span class="card-title">{{apartment.addresses.address}}</span>
+        </div>
   </div>
 </template>
 
@@ -10,6 +14,7 @@
 
 export default {
     name: "ApartmentCard",
+    props: ["apartment"],
 }
 </script>
 
--- resources/js/components/apartments/ApartmentsList.vue
+++ resources/js/components/apartments/ApartmentsList.vue
@@ -1,5 +1,9 @@
 <template>
-    <ApartmentCard/>
+    <div class="container">
+          <div class="row justify-content-center mt-5">
+                <ApartmentCard :v-if="apartments.visible = 1" v-for="apartment in apartments" :key="apartment.id" :apartment="apartment"/>
+          </div>
+    </div>
 </template>
 
 <script>
@@ -23,7 +27,8 @@ export default {
     created() {
         axios.get(`http://127.0.0.1:8000/api/apartments`)
             .then(response => {
-                console.log(response);
+                this.apartments = response.data;
+                console.log(this.apartments)
             })
         .catch(e => {
         this.errors.push(e);
