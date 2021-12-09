b3ef07a3d0a6b8f1db7ad3a23a594d7d8822236d
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-09 14:21:21 +0100

    implementing footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,35 @@
 <footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-md-4">
 
-    <h1> footerWIP</h1>
+            </div>
+            <div class="col-6 col-md-2">
+                <h4 class="yellow">Titoletto</h4>
+                <ul>
+                    <li><a href="#">test</a></li>
+                    <li><a href="#">test</a></li>
+                    <li><a href="#">test</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-2">
+                <h4 class="yellow">Titoletto</h4>
+                <ul>
+                    <li><a href="#">test</a></li>
+                    <li><a href="#">test</a></li>
+                    <li><a href="#">test</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-2">
+                <h4 class="yellow">Titoletto</h4>
+                <ul>
+                    <li><a href="#">test</a></li>
+                    <li><a href="#">test</a></li>
+                    <li><a href="#">test</a></li>
+                </ul>
+            </div>
+            
+        </div>
+    </div>
+
 </footer>
