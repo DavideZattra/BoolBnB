0d8ef0d708c0550302ea6ecee37ee4559a3f6764
Davide Zattra <d.zattra@gmail.com>
2021-12-09 20:33:13 +0100

    fixed spacing

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -5,7 +5,11 @@
 
     <div class="row justify-content-center">
 
-        <div class="col-12 text-center">My Dashboard</div>
+        {{-- <div class="col-12 text-center text-white">
+            <h2 class="">
+                My Dashboard
+            </h2>
+        </div> --}}
 
         {{-- Profile information --}}
         <div class="col-12 col-md-6">
