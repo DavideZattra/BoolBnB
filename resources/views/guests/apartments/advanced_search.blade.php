55925994177a462e1079d0d5d36e377398af33c4
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-10 12:48:46 +0100

    implementing map

--- resources/views/guests/apartments/advanced_search.blade.php
+++ resources/views/guests/apartments/advanced_search.blade.php
@@ -1,8 +1,16 @@
 @extends('layouts.app')
 
+@section('cdn-entrypoint')
+    <link  rel='stylesheet'  type='text/css'  href='https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.64.0/maps/maps.css'>
+@endsection
+
 @section('content')
 
     {{-- Vue container --}}
     <div id="root"></div>
-
+    @endsection
+    
+@section('scripts-entrypoint')
+    <script  src="https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.64.0/maps/maps-web.min.js"></script> 
+    <script  src="https://api.tomtom.com/maps-sdk-for-web/cdn/5.x/5.64.0/services/services-web.min.js"></script> 
 @endsection
