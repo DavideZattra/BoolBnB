f37b9a08cdffb54e39569a68e107b373950b4b33
Camilla Manzo <85356516+camillamanzo@users.noreply.github.com>
2021-12-10 12:10:44 +0100

    finalized dashboard

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -5,79 +5,57 @@
 
     <div class="row justify-content-center">
 
-        {{-- <div class="col-12 text-center text-white">
+        <div class="col-12 pb-4">
             <h2 class="">
                 My Dashboard
             </h2>
-        </div> --}}
+        </div>
 
         {{-- Profile information --}}
         <div class="col-12 col-md-6">
-
             <div class="card">
 
                 <div class="card-header ">My Profile</div>
-
-                <div class="card-body d-md-flex ">
-
+                <div class="card-body d-md-flex align-items-center">
                     <div class="col-12 col-md-6 d-flex align-items-center">
-                        
                         <div class="profile-pic">
-
                             <img src="{{ asset('storage/'. $user->profile_picture) }}" alt="" >
-
                         </div>
-
                     </div>
 
                     <div class="user-data col-12 col-md-6">
                         <ul>
-
-                            <li>Name: {{ $user->name }}</li>
-                            <li>Surname: {{ $user->surname }}</li>
-                            <li>Email: {{ $user->email }}</li>
-                            <li>Birth date: {{ $user->birth_date }}</li>
-
+                            <li><span>Name:</span> {{ $user->name }}</li>
+                            <li><span>Surname:</span> {{ $user->surname }}</li>
+                            <li><span>Email:</span> {{ $user->email }}</li>
+                            <li><span>Birth date:</span> {{ $user->birth_date }}</li>
                         </ul>
                     </div>
-
                 </div>
 
                 <div class="card-footer">
-                    
                     {{-- Button to edit the user profile --}}
                     <a href='{{ route('users.edit', Auth::user()->id) }}' class="btn btn-dark">
                         Edit your profile
                     </a>
-                    
                 </div>
 
             </div>
-
         </div>
+
         {{-- apartments information --}}
         <div class="col-12 col-md-6">
-
             <div class="card">
 
                 <div class="card-header">My Apartments</div>
-
                 <div class="card-body">
-
-                    <ul class="list-group list-group-flush">
-    
+                    <ul class="apartment-list">
                         @forelse ($userApartments as $apartment)
-                            
-                        <li class="list-group-item">{{ $apartment['descriptive_title'] }}</li>
-    
+                            <li class="list-group-item">{{ $apartment['descriptive_title'] }}</li>
                         @empty
-                            
-                        <li class="list-group-item">Insert a new apartment and become a member of our family!</li>
-    
+                            <li class="list-group-item">Insert a new apartment and become a member of our family!</li>
                         @endforelse
-    
                     </ul>
-                    
                 </div>
 
                 <div class="card-footer d-flex justify-content-between">
@@ -85,12 +63,10 @@
                     <a href='{{ route('users.apartments.index')}}' class="btn btn-dark">
                         My Apartments
                     </a>
-
                     {{-- Button that allow user to insert a new apartment --}}
                     <a href='{{ route('users.apartments.create')}}' class="btn btn-dark">
                         Insert new apartment
                     </a>
-
                 </div>
 
             </div>
