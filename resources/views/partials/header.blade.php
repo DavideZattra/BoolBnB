826961fb4ed67759bdf8f9078b370ace26f19c23
Camilla Manzo <85356516+camillamanzo@users.noreply.github.com>
2021-12-10 12:40:05 +0100

    added route to dashboard in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -21,7 +21,7 @@
                 <ul class="navbar-nav mx-auto">
                     {{-- <li><input class="form-control my_search-bar pl-3" type="search" placeholder="Search" aria-label="Search"></li> --}}
                     <li class="mx-3"><a class="nav-link"  href="#">Find a house</a></li>
-                    <li class="mx-3"><a class="nav-link"  href="#">Dashboard</a></li>
+                    <li class="mx-3"><a class="nav-link"  href="{{route('users.dashboard')}}">Dashboard</a></li>
                     <li class="mx-3"><a class="nav-link"  href="#">Host</a></li>
                 </ul>
     
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -14,48 +14,34 @@
     @endif 
     
     <form action="{{ route('users.update')}}" method="POST" enctype="multipart/form-data">
-        @csrf
         
+        @csrf
         @method('patch')
 
         <div class="form-group">
-
             <label for="name">Modify your name</label>
             <input class="form-control" type="text" id="name" name="name" placeholder="Insert your name" value="{{ $user->name }}" required>
-            
         </div>
 
         <div class="form-group">
-
             <label for="surname">Modify your surname</label>
             <input class="form-control" type="text" id="surname" name="surname" placeholder="Insert your surname" value="{{ $user->surname }}" required>
-            
         </div>
 
         <div class="form-group">
-
             <label for="email">Modify your email</label>
             <input class="form-control" type="email" id="email" name="email" placeholder="Insert your email" value="{{ $user->email }}" required>
-            
         </div>
 
         <div class="form-group">
-
             <label for="birth_date">Modify your birth date</label>
             <input class="form-control" type="date" id="birth_date" name="birth_date" placeholder="Insert your birth date" value="{{ $user->birth_date }}" required>
-            
         </div>
 
         <div class="form-group">
-            
             <label for="profile_picture">Upload your profile picture</label>
             <input class="form-control" type="file" id="profile_picture" name="profile_picture" >
-            
         </div>
-
-        
-
-        
         
         <button class="btn btn-warning" type="submit">Modify your profile</button>
         
