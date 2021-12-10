c52ed9aca5ca0d69e62af73c463e6d14b234504d
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-10 10:49:13 +0100

    aggiunto map.vue

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,15 +1,20 @@
 <template>
-    <ApartmentsList/>
+    <div>
+        <ApartmentsList/>
+        <Map/>
+    </div>
 </template>
 
 <script>
 
 import ApartmentsList from './apartments/ApartmentsList.vue';
+import Map from './apartments/Map.vue';
 
 export default {
     name: "App",
     components: {
         ApartmentsList,
+        Map
     },
 }
 </script>
