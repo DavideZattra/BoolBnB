6dd589aad1502d66f24b93cb9df6aae99051a654
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-11 16:01:51 +0100

    fixed jumbotron

--- resources/views/guests/home/jumbotron.blade.php
+++ resources/views/guests/home/jumbotron.blade.php
@@ -2,7 +2,7 @@
     <div id="jumbo" class="d-flex justify-content-end align-items-center">
 
         {{-- div only visible when > sm --}}
-        <div class="jumbo-text jumbo-lg d-none d-md-block text-center">
+        <div class="jumbo-text jumbo-lg d-none d-lg-block text-center">
             <h1>Dream. Travel. Live.</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, itaque a tempore similique blanditiis magni nam neque quis praesentium enim quaerat quo. Explicabo tempore ullam commodi sed pariatur, dignissimos nemo!</p>
             <div class="jumbo-button">
@@ -18,19 +18,4 @@
 
     </div>
 
-    {{-- div only visible when <= sm --}}
-    <div class="jumbo-text jumbo-sm d-block d-md-none text-center">
-        <h1>Dream. Travel. Live.</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, itaque a tempore similique blanditiis magni nam neque quis praesentium enim quaerat quo. Explicabo tempore ullam commodi sed pariatur, dignissimos nemo!</p>
-        <div class="jumbo-button">
-
-            {{-- added route (if user::auth->index/ else->login ) --}}
-            @if (Auth::check())
-                    <button type="button" class="btn btn-sm"><a href="{{route("users.dashboard")}}">My profile</a></button>
-            @else
-                <button type="button" class="btn btn-sm"><a href="{{route("login")}}">Create a profile.</a></button>
-            @endif
-        </div>
-    </div>
-    
 </section>
