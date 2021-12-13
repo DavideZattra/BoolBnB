fbc154a7fb19f8c231902745d7c8b2484ebb8f1c
Camilla Manzo <85356516+camillamanzo@users.noreply.github.com>
2021-12-13 11:46:28 +0100

    added margin

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -39,7 +39,7 @@
                             </li>
                         @endif
                     @else
-                        <li class="nav-item dropdown">
+                        <li class="nav-item dropdown ml-3">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle no-border" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>
