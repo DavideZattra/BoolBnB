d00754fd0fc7bdb0b854c3ca220e22292916e045
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-14 12:22:10 +0100

    aggiunto slider

--- resources/views/guests/home/promotedHouses.blade.php
+++ resources/views/guests/home/promotedHouses.blade.php
@@ -4,9 +4,34 @@
 @endsection
 
 <section id="promoted-houses">
+    <h2>The best houses you will find today</h2>
     <div class="main-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
-        <div class="carousel-cell">carousel</div>
-        <div class="carousel-cell">carousel</div>
-        <div class="carousel-cell">carousel</div>
+        <div class="carousel-cell">
+            <a href="#"><img src="{{ asset('img/example-house_1.jpeg') }}" alt="House image"></a>
+        </div>
+        <div class="carousel-cell">
+            <a href="#"><img src="{{ asset('img/example-house_2.jpg') }}" alt="House image"></a>
+        </div>
+        <div class="carousel-cell">
+            <a href="#"><img src="{{ asset('img/example-house_3.jpg') }}" alt="House image"></a>
+        </div>
+        <div class="carousel-cell">
+            <a href="#"><img src="{{ asset('img/example-house_4.jpg') }}" alt="House image"></a>
+        </div>
+        <div class="carousel-cell">
+            <a href="#"><img src="{{ asset('img/example-house_5.jpg') }}" alt="House image"></a>
+        </div>
+        <div class="carousel-cell">
+            <a href="#"><img src="{{ asset('img/example-house_6.jpg') }}" alt="House image"></a>
+        </div>
+        <div class="carousel-cell">
+            <a href="#"><img src="{{ asset('img/example-house_7.jpg') }}" alt="House image"></a>
+        </div>
+        <div class="carousel-cell">
+            <a href="#"><img src="{{ asset('img/example-house_8.webp') }}" alt="House image"></a>
+        </div>
+        <div class="carousel-cell">
+            <a href="#"><img src="{{ asset('img/Jose-Jose.jpg') }}" alt="House image"></a>
+        </div>
     </div>
 </section>
