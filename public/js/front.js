2b91142107cbeaf5305e3d35d7b5a2de64dd5ec6
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-14 16:05:06 +0100

    modifiche db per inserimento manuale

--- public/js/front.js
+++ public/js/front.js
@@ -2686,6 +2686,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "SearchAndFilter",
   props: ['amenities'],
@@ -4098,6 +4099,7 @@ var render = function () {
             ],
             staticClass: "form-control",
             attrs: {
+              value: "",
               placeholder: "Choose an address or a city",
               type: "text",
               "aria-label": "Small",
