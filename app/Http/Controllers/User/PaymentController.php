4339b6efb1c31f87af69fbc3da1b364c5d2451c6
Camilla Manzo <85356516+camillamanzo@users.noreply.github.com>
2021-12-15 14:53:57 +0100

    trying to fix controller

--- app/Http/Controllers/User/PaymentController.php
+++ app/Http/Controllers/User/PaymentController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Carbon\Carbon;
 use App\Models\Sponsor;
+use App\Models\Apartment;
 use App\User;
 
 class PaymentController extends Controller
@@ -41,60 +42,35 @@ class PaymentController extends Controller
             'privateKey' => '5950ef3e257855ac1bcdf4a0c64ea5c2'
         ]);
 
-        // $amount = $request->amount;
-
-        $data = $request->all();
-        $amount = Sponsor::where('id',$data['sponsor_id'])->pluck('price')->first();
-
-        dd($amount);
+        $amount = $request->amount;
         $nonce = $request->payment_method_nonce;
-        $name = $user->name;
 
         $result = $gateway->transaction()->sale([
             'amount' => $amount,
             'paymentMethodNonce' => $nonce,
+            'customer' => [
+                'firstName' => 'Alessandro',
+                'lastName' => 'Sainato',
+                'email' => 'alessandro.sainato@boolean.com',
+            ],
             'options' => [
                 'submitForSettlement' => true
             ]
         ]);
-
-        // if ($result->success) {
-        //     $transaction = $result->transaction;
-        //     // header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id);
-        //     // return back()->with('success_message', 'Transaction succesSful. The ID is'. $transaction->id);
-        //     $ora = Carbon::now();
-        //     $ultimaDataFine = Carbon::parse(Sponsor::where('apartment_id',$request->apartment_id)->pluck('end_date')->sortDesc()->first());
-        //     if($ultimaDataFine->greaterThan($ora)){
-        //         $request['data_inizio'] = $ultimaDataFine;
-        //     } else {
-        //         $request['data_inizio'] = $ora;
-        //     };
-        //     // aggiungo durata alla data inizio e calcolo data fine
-        //     $durata = Sponsor::where('id',$request->sponsor_id)->pluck('durata')->first();
-        //     $request['data_fine'] = Carbon::parse($request['data_inizio'])->addHours($durata);
-        //     // scrivo dati su database e restituisco JSON di risposta
-        //     $sponsorApartment = SponsorApartment::create($request->all());
-        //     return redirect()->route('apartments.show', $data['apartment_id'])->with('success_message', 'Pagamento effettuato. Transazione n. '. $transaction->id);
-        // } else {
-        //     $errorString = "";
-  
-        //     foreach($result->errors->deepAll() as $error) {
-        //         $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
-        //     }
-  
-        //     // $_SESSION["errors"] = $errorString;
-        //     // header("Location: " . $baseUrl . "index.php");
-        //     return back()->withErrors('An error occured with message '. $result->message);
-        // }
-    
         if ($result->success) {
             $transaction = $result->transaction;
 
             
-            $order = new Sponsor();
+            // $order = new Order();
             $data =  $request->all();
+
+            $data['restaurant_id'] = (int)$data['restaurant_id'];   
+            $data['total_price'] = $data['amount'];
+
+            // $order->fill($data);
+            // $order->save();
     
-            return view('users.braintree.checkout')->with('success_message', 'The payment was successfully. The id:'. $transaction->id);
+            return view('payment.checkout')->with('success_message', 'The payment was successfully. The id:'. $transaction->id);
         } else {
             $errorString = "";
     
--- resources/views/users/Braintree/payment.blade.php
+++ resources/views/users/Braintree/payment.blade.php
@@ -19,33 +19,14 @@
         </div>
     @endif
 
-    {{-- <form method="post" id="payment-form" action="{{ route('users.braintree.checkout') }}">
-        @csrf
-        <section>
-            <label for="amount">
-                <span class="input-label">Amount</span>
-                <div class="input-wrapper amount-wrapper">
-                    <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
-                </div>
-            </label>
-
-            <div class="bt-drop-in-wrapper">
-                <div id="bt-dropin"></div>
-            </div>
-        </section>
-
-        <input id="nonce" name="payment_method_nonce" type="hidden" />
-        <button class="button" type="submit"><span>Test Transaction</span></button>
-    </form> --}}
-
     <form method="post" id="payment-form" action="{{ route('users.braintree.checkout') }}">
         @csrf
         <label for="amount">
             <span class="input-label">Amount</span>
             <div class="input-wrapper amount-wrapper">
-                <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
-                <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
-                <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
+                {{-- @foreach ($sponsors as $sponsor) --}}
+                    <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
+                {{-- @endforeach --}}
             </div>
         </label>
         <div id="dropin-container"></div>
