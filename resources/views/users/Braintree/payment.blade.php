c6bd8d5d9969af3898786dabf3f06aeeaee147a3
Camilla Manzo <85356516+camillamanzo@users.noreply.github.com>
2021-12-15 10:59:29 +0100

    added other script

--- resources/views/users/Braintree/payment.blade.php
+++ resources/views/users/Braintree/payment.blade.php
@@ -19,35 +19,47 @@
 
     <script type="text/javascript">
 
-        const form = document.getElementById('payment-form');
-        // Step two: create a dropin instance using that container (or a string
-        //   that functions as a query selector such as `#dropin-container`)
+        var button = document.querySelector('#submit-button');
+
         braintree.dropin.create({
-            container: document.getElementById('dropin-container'),
-            authorization: CLIENT_TOKEN_FROM_SERVER,
-            container: '#dropin-container'
-            }, 
-            
-            (error, dropinInstance) => {
-
-            if (error) console.error(error);
-
-            form.addEventListener('submit', event => {
-            event.preventDefault();
-
-            dropinInstance.requestPaymentMethod((error, payload) => {
-            if (error) console.error(error);
-
-            // Step four: when the user is ready to complete their
-            //   transaction, use the dropinInstance to get a payment
-            //   method nonce for the user's selected payment method, then add
-            //   it a the hidden field before submitting the complete form to
-            //   a server-side integration
-            document.getElementById('nonce').value = payload.nonce;
-            form.submit();
-                });
+        authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+        selector: '#dropin-container'
+        }, function (err, instance) {
+        button.addEventListener('click', function () {
+            instance.requestPaymentMethod(function (err, payload) {
+            // Submit payload.nonce to your server
             });
+        })
         });
+        // const form = document.getElementById('payment-form');
+        // // Step two: create a dropin instance using that container (or a string
+        // //   that functions as a query selector such as `#dropin-container`)
+        // braintree.dropin.create({
+        //     container: document.getElementById('dropin-container'),
+        //     authorization: CLIENT_TOKEN_FROM_SERVER,
+        //     container: '#dropin-container'
+        //     }, 
+            
+        //     (error, dropinInstance) => {
+
+        //     if (error) console.error(error);
+
+        //     form.addEventListener('submit', event => {
+        //     event.preventDefault();
+
+        //     dropinInstance.requestPaymentMethod((error, payload) => {
+        //     if (error) console.error(error);
+
+        //     // Step four: when the user is ready to complete their
+        //     //   transaction, use the dropinInstance to get a payment
+        //     //   method nonce for the user's selected payment method, then add
+        //     //   it a the hidden field before submitting the complete form to
+        //     //   a server-side integration
+        //     document.getElementById('nonce').value = payload.nonce;
+        //     form.submit();
+        //         });
+        //     });
+        // });
 
     </script>
 
