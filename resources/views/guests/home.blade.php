827ae8af65606c161c801227e8f030cbd5cf8165
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-16 11:24:56 +0100

    aggiunte sezioni dream-travel-live

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -3,5 +3,8 @@
 @section('content')
     @include('guests.home.jumbotron')
     @include('guests.home.promotedHouses')
+    @include('guests.home.dream')
     @include('guests.home.houses')
+    @include('guests.home.travel')
+    @include('guests.home.live')
 @endsection
