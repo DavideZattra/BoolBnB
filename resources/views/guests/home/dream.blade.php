512ea3e007ac40f1c120e3dcde56665fca52ede6
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-17 15:14:09 +0100

    aggiunto modifiche dream travel live

--- resources/views/guests/home/dream.blade.php
+++ resources/views/guests/home/dream.blade.php
@@ -1,8 +1,12 @@
 <section id="dtl">
     <div class="container-fluid">
         <div class="row">
+            <div class="mobile-title">
+                <h2>Dream.</h2>
+                <p>Sleep in fantastic locations where you can admire wonderful landscapes, placed on the edge of your most beautiful dreams.</p>
+            </div>
             <div class="col-12 col-lg-8 dtl-container">
-                <img src="{{ asset('img/dream.jpg') }}" alt="image dream">
+                <img class="rainbow-shadow" src="{{ asset('img/dream.jpg') }}" alt="image dream">
             </div>
             <div class="col-lg-4 dtl-container d-flex justify-content-end align-items-center dtl-text__content scroll">
                 <div class="right-side">
--- resources/views/guests/home/travel.blade.php
+++ resources/views/guests/home/travel.blade.php
@@ -1,14 +1,18 @@
 <section id="dtl">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 col-lg-8 dtl-container">
-                <img src="{{ asset('img/travel.jpg') }}" alt="image dream">
+            <div class="mobile-title">
+                <h2>Travel.</h2>
+                <p>Sleep in fantastic locations where you can admire wonderful landscapes, placed on the edge of your most beautiful dreams.</p>
             </div>
             <div class="col-lg-4 dtl-container d-flex justify-content-end align-items-center dtl-text__content scroll">
-                <div class="right-side">
+                <div class="">
                     <h2>Travel.</h2>
                     <p>Sleep in fantastic locations where you can admire wonderful landscapes, placed on the edge of your most beautiful dreams.</p>
                 </div>
+            </div>
+            <div class="col-12 col-lg-8 dtl-container">
+                <img class="rainbow-shadow" src="{{ asset('img/travel.jpg') }}" alt="image dream">
             </div>
         </div>
     </div>
