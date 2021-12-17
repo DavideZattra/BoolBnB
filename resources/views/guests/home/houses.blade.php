4f4bcfab93716f93a72ab4184361123c1d5de2df
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-17 18:17:39 +0100

    modificato cards con filter

--- resources/views/guests/home/houses.blade.php
+++ resources/views/guests/home/houses.blade.php
@@ -1,26 +1,60 @@
 <section class="houses bg-home-section">
     <h2 class="text-center font-weight-bold mb-5">Some city you must visit once in your life...</h2>
     <div class="container scroll">
-        <a href="{{ url('/search') }}">
-            <div class="row mb-4">
-                <div class="col-6 col-md-4 t-ideas"><img src="{{ asset('img/roma.jpeg') }}" alt=""></div>
-                <div class="col-6 col-md-5 t-ideas"><img src="{{ asset('img/dubai.jpg') }}" alt=""></div>
-                <div class="col-4 col-md-3 t-ideas d-none d-md-block"><img src="{{ asset('img/bruxelles.jpg') }}" alt=""></div>
+
+        <div class="row mb-3">
+            <div class="col-6 col-md-4 city-filter">
+                <div class="roma t-ideas">
+                    <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Roma</h1></a></div>
+                </div>
             </div>
-        </a>
-        <a href="{{ url('/search') }}">
-            <div class="row mb-4">
-                <div class="col-6 col-md-3 t-ideas"><img src="{{ asset('img/amsterdam.jpg') }}" alt=""></div>
-                <div class="col-6 col-md-3 t-ideas"><img src="{{ asset('img/copenaghen.jpg') }}" alt=""></div>
-                <div class="col-md-6 t-ideas d-none d-md-block"><img src="{{ asset('img/venezia.jpg') }}" alt=""></div>
+            <div class="col-6 col-md-5 city-filter">
+                <div class="dubai t-ideas">
+                    <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Dubai</h1></a></div>
+                </div>
             </div>
-        </a>
-        <a href="{{ url('/search') }}">
-            <div class="row mb-4">
-                <div class="col-6 col-md-5 t-ideas"><img src="{{ asset('img/tokyo.jpg') }}" alt=""></div>
-                <div class="col-6 col-md-4 t-ideas"><img src="{{ asset('img/vienna.jpg') }}" alt=""></div>
-                <div class="col-md-3 t-ideas d-none d-md-block"><img src="{{ asset('img/barcellona.jpg') }}" alt=""></div>
+            <div class="col-4 col-md-3 d-none d-md-block city-filter">
+                <div class="bruxelles t-ideas">
+                    <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Bruxelles</h1></a></div>               
+                </div>
             </div>
-        </a>
+        </div>   
+
+        <div class="row mb-3">
+            <div class="col-6 col-md-3 city-filter">
+                <div class="amsterdam t-ideas">
+                    <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Amsterdam</h1></a></div>                 
+                </div>
+            </div>
+            <div class="col-6 col-md-3 city-filter">
+                <div class="copenaghen t-ideas">
+                    <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Copenaghen</h1></a></div>          
+                </div>
+            </div>
+            <div class="col-md-6 d-none d-md-block city-filter">
+                <div class="venezia t-ideas">
+                    <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Venezia</h1></a></div>             
+                </div>
+            </div>
+        </div>
+
+        <div class="row mb-3">
+            <div class="col-6 col-md-5 city-filter">
+                <div class="tokyo t-ideas">
+                    <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Tokyo</h1></a></div>                
+                </div>
+            </div>
+            <div class="col-6 col-md-4 city-filter">
+                <div class="vienna t-ideas">
+                    <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Vienna</h1></a></div>                   
+                </div>
+            </div>
+            <div class="col-md-3 d-none d-md-block city-filter">
+                <div class="barcellona t-ideas">
+                    <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Barcellona</h1></a></div>              
+                </div>
+            </div>
+        </div>  
+
     </div>
 </section>
