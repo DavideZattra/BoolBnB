db45f9842cfa9c91493d3a3c6c6a905ab895796b
Furentsu <eugeniofranzoso@gmail.com>
2021-12-17 15:17:18 +0100

    Added radio buttons to payments' page

--- resources/views/users/braintree/payment.blade.php
+++ resources/views/users/braintree/payment.blade.php
@@ -2,43 +2,66 @@
 
 @section('content')
     
-    @if (session('success_message'))
-        <div class="alert alert-success">
-            {{ session('success_message') }}
-        </div>
-    @endif
+    <section class="bg-container">
+        @if (session('success_message'))
+            <div class="alert alert-success">
+                {{ session('success_message') }}
+            </div>
+        @endif
       
-    @if(count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+        @if(count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-    <div class="content">
-        <form method="post" id="payment-form" action="{{route('users.braintree.checkout', $apartment)}}">
-            @csrf
-            
-            <section>
-                <label for="amount">
-                    <span class="input-label">Amount</span>
-                    <div class="input-wrapper amount-wrapper">
-                        <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="10">
-                    </div>
-                </label>
+        <div class="content container-flex d-flex justify-content-center p-5">
+            <form method="post" id="payment-form" action="{{route('users.braintree.checkout', $apartment)}}">
+                @csrf 
 
                 <div class="bt-drop-in-wrapper">
                     <div id="bt-dropin"></div>
                 </div>
-            </section>
 
-            <input id="nonce" name="payment_method_nonce" type="hidden" />
-            <button class="button" type="submit"><span>Test Transaction</span></button>
-        </form>
-    </div>
+                <div class="form-check mt-4">
+                    
+                    <div class="input-wrapper amount-wrapper">
+                        <input class="form-check-input" type="radio" name="amount" min="1" id="amount" value="2.99" checked>
+                        <label class="form-check-label text-white" for="amount">
+                            Standard - $2.99
+                        </label>
+                    </div>
+
+                    <div class="input-wrapper amount-wrapper">
+                        <input class="form-check-input" type="radio" name="amount" min="1" id="amount" value="5.99">
+                        <label class="form-check-label text-white" for="amount">
+                            Gold - $5.99
+                        </label>
+                    </div>
+
+                    <div class="input-wrapper amount-wrapper">
+                        <input class="form-check-input" type="radio" name="amount" min="1" id="amount" value="9.99">
+                        <label class="form-check-label text-white" for="amount">
+                            Platinum - $9.99
+                        </label>
+                    </div>
+                    
+                  </div>
+
+                <input id="nonce" name="payment_method_nonce" type="hidden" />
+
+                <div class="payment-button d-flex">
+                    <button class="btn btn-outline-secondary text-white mt-4 flex-grow-1" type="submit"><span>Pay</span></button>
+                </div>
+                
+            </form>
+        </div>
+    </section>
+    
 
     <script src="https://js.braintreegateway.com/web/dropin/1.13.0/js/dropin.min.js"></script>
     <script>
