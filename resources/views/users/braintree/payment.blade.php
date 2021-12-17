d2ec1f34e067adb95a73e0e67fbe9effb2df644d
Furentsu <eugeniofranzoso@gmail.com>
2021-12-17 16:14:59 +0100

    Changed payments' page layout structure and data display on statistics' page

--- resources/views/users/braintree/payment.blade.php
+++ resources/views/users/braintree/payment.blade.php
@@ -19,43 +19,45 @@
             </div>
         @endif
 
-        <div class="content container-flex d-flex justify-content-center p-5">
+        <div class="content container-flex p-5">
             <form method="post" id="payment-form" action="{{route('users.braintree.checkout', $apartment)}}">
                 @csrf 
 
-                <div class="bt-drop-in-wrapper">
-                    <div id="bt-dropin"></div>
-                </div>
 
-                <div class="form-check mt-4">
+                <div class="form-check mt-4 row w-100 d-flex justify-content-between">
                     
-                    <div class="input-wrapper amount-wrapper">
+                    <div class="input-wrapper amount-wrapper col-4 text-center">
+
                         <input class="form-check-input" type="radio" name="amount" min="1" id="amount" value="2.99" checked>
                         <label class="form-check-label text-white" for="amount">
                             Standard - $2.99
                         </label>
                     </div>
 
-                    <div class="input-wrapper amount-wrapper">
+                    <div class="input-wrapper amount-wrapper col-4 text-center">
                         <input class="form-check-input" type="radio" name="amount" min="1" id="amount" value="5.99">
                         <label class="form-check-label text-white" for="amount">
                             Gold - $5.99
                         </label>
                     </div>
 
-                    <div class="input-wrapper amount-wrapper">
+                    <div class="input-wrapper amount-wrapper col-4 text-center">
                         <input class="form-check-input" type="radio" name="amount" min="1" id="amount" value="9.99">
                         <label class="form-check-label text-white" for="amount">
                             Platinum - $9.99
                         </label>
                     </div>
                     
-                  </div>
+                </div>
+                
+                <div class="bt-drop-in-wrapper d-flex justify-content-center w-100 mt-5">
+                    <div id="bt-dropin"></div>
+                </div>
 
                 <input id="nonce" name="payment_method_nonce" type="hidden" />
 
-                <div class="payment-button d-flex">
-                    <button class="btn btn-outline-secondary text-white mt-4 flex-grow-1" type="submit"><span>Pay</span></button>
+                <div class="payment-button w-100 d-flex justify-content-center">
+                    <button class="btn btn-outline-secondary text-white mt-4" type="submit"><span>Pay</span></button>
                 </div>
                 
             </form>
