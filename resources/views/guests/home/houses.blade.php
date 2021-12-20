2e0e38487f3b863937262e5f234cd7b488cf03c0
Giovanni Gualtieri <gualtierigio@gmail.com>
2021-12-20 11:34:43 +0100

    animation

--- resources/views/guests/home/houses.blade.php
+++ resources/views/guests/home/houses.blade.php
@@ -40,7 +40,7 @@
 
         <div class="row mb-3">
             <div class="col-6 col-md-5 city-filter">
-                <div class="tokyo t-ideas">
+                <div class="barcellona t-ideas">
                     <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Tokyo</h1></a></div>                
                 </div>
             </div>
@@ -50,7 +50,7 @@
                 </div>
             </div>
             <div class="col-md-3 d-none d-md-block city-filter">
-                <div class="barcellona t-ideas">
+                <div class="tokyo t-ideas">
                     <div class="city-title"><a href="{{ route('apartments.search') }}"><h1>Barcellona</h1></a></div>              
                 </div>
             </div>
